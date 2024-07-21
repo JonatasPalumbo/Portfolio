d783b9cf8f4b64a5ad93b33a80dcd0df13929b5d
JonatasPalumbo <jonataspalumbo@gmail.com>
2024-07-21 20:46:47 -0300

    [Blog] New article

--- src/portfolio.js
+++ src/portfolio.js
@@ -320,16 +320,10 @@ const blogSection = {
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://medium.com/@jonataspalumbo/how-big-is-the-number-2%C2%B2%E2%81%B5%E2%81%B6-b04eeb833a1e",
+      title: "How Big is the Number 2²⁵⁶",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+        "Unveiling the Magnitude and Security Implications of 2²⁵⁶ in Cryptography."
     }
   ],
   display: true // Set false to hide this section, defaults to true
